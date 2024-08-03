5c1c4e5a111efeaadc6fe1cd8180b8ca65be753f
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-03 22:09:01 +0600

    added sonner

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const { register, handleSubmit } = useForm({
     // This is set default just to reduce the pain of writing data all the time while testing. This should be removed before production
     defaultValues: {
@@ -18,17 +21,23 @@ const Login = () => {
 
   const [login] = useLoginMutation();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: FieldValues) => {
+    const loginToastId = toast.loading("Logging In");
     const userInfo = {
       id: data.userId,
       password: data.password,
     };
 
-    const res = await login(userInfo).unwrap(); //here .unwrap() Unwraps a mutation call and provide the raw response.
-    console.log(res);
-    const user = verifyToken(res.data.accessToken);
-    console.log(user);
-    dispatch(setUser({ user: user, token: res.data.accessToken }));
+    try {
+      const res = await login(userInfo).unwrap(); //here .unwrap() Unwraps a mutation call and provide the raw response.
+      const user = verifyToken(res.data.accessToken) as TUser;
+      console.log(user);
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("logged In", { id: loginToastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong", { id: loginToastId, duration: 2000 });
+    }
   };
 
   return (
