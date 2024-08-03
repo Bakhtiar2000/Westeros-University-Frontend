1ce9d3e2c2e8799266e540a30589417caf4abf1a
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-03 18:39:23 +0600

    added redux-persist

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    // This is set default just to reduce the pain of writing data all the time while testing. This should be removed before production
+    defaultValues: {
+      userId: "A-0001",
+      password: "12345",
+    },
+  });
+
+  const [login] = useLoginMutation();
+
+  const onSubmit = async (data: any) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+
+    const res = await login(userInfo).unwrap(); //here .unwrap() Unwraps a mutation call and provide the raw response.
+    console.log(res);
+    const user = verifyToken(res.data.accessToken);
+    console.log(user);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
   return (
-    <div>
-      <p>Login</p>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+      {/*  // In ant design, type keyword is over-written as htmlType */}
+    </form>
   );
 };
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import { baseApi } from "./api/baseApi";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistAuthReducer,
   },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
