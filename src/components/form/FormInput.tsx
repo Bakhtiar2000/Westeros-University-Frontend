e4bf4064a67b59984dbec71bb32ce273e6093949
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-12 12:22:08 +0600

    completed reusableForm

--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,12 +1,21 @@
-import { useFormContext } from "react-hook-form";
+import { Input } from "antd";
+import { Controller } from "react-hook-form";
 
-const FormInput = ({ type, name, label }) => {
-  const { register } = useFormContext();
+type TInputProps = {
+  type: string;
+  name: string;
+  label?: string;
+};
+
+const FormInput = ({ type, name, label }: TInputProps) => {
   return (
-    <>
+    <div style={{ marginBottom: "20px" }}>
       {label && <label htmlFor={name}>{label}: </label>}
-      <input type={type} id={name} {...register(name)} />
-    </>
+      <Controller
+        name={name} // registered the field here
+        render={({ field }) => <Input {...field} type={type} id={name} />}
+      />
+    </div>
   );
 };
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Button } from "antd";
-import { FieldValues, useForm, useFormContext } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser, TUser } from "../redux/features/auth/authSlice";
@@ -13,47 +13,41 @@ import FormInput from "../components/form/FormInput";
 const Login = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { register } = useForm({
-    // This is set default just to reduce the pain of writing data all the time while testing. This should be removed before production
-    defaultValues: {
-      userId: "A-0001",
-      password: "12345",
-    },
-  });
+
+  const defaultValues = {
+    userId: "A-0001",
+    password: "12345",
+  };
 
   const [login] = useLoginMutation();
-
   const onSubmit = async (data: FieldValues) => {
     console.log(data);
-    // const loginToastId = toast.loading("Logging In");
-    // const userInfo = {
-    //   id: data.userId,
-    //   password: data.password,
-    // };
+    const loginToastId = toast.loading("Logging In");
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
 
-    // try {
-    //   const res = await login(userInfo).unwrap(); //here .unwrap() Unwraps a mutation call and provide the raw response.
-    //   const user = verifyToken(res.data.accessToken) as TUser;
-    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
-    //   toast.success("logged In", { id: loginToastId, duration: 2000 });
-    //   navigate(`/${user.role}/dashboard`);
-    // } catch (error) {
-    //   toast.error("Something went wrong", { id: loginToastId, duration: 2000 });
-    // }
+    try {
+      const res = await login(userInfo).unwrap(); //here .unwrap() Unwraps a mutation call and provide the raw response.
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("logged In", { id: loginToastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong", { id: loginToastId, duration: 2000 });
+    }
   };
 
   return (
-    <UniForm onSubmit={onSubmit}>
-      <div>
-        <FormInput type="text" name="userId" label="Id: " />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <FormInput type="password" name="password" label="Password: " />
-      </div>
-      <Button htmlType="submit">Login</Button>
-      {/*  // In ant design, type keyword is over-written as htmlType */}
-    </UniForm>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <UniForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <FormInput type="text" name="userId" label="Id" />
+        <FormInput type="password" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+        {/*  // In ant design, type keyword is over-written as htmlType */}
+      </UniForm>
+    </Row>
   );
 };
 
