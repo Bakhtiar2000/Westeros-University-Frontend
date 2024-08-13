615667d0a10f092921b541532b2af8f665b863e8
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-14 01:13:02 +0600

    added select element

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,8 +1,24 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import FormInput from "../../../components/form/FormInput";
+import UniForm from "../../../components/form/UniForm";
+import { Button, Col, Flex } from "antd";
+import FormSelect from "../../../components/form/FormSelect";
+
 const CreateAcademicSemester = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h4>CreateAcademicSemester</h4>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <UniForm onSubmit={onSubmit}>
+          <FormInput type="text" name="name" label="name" />
+          <FormInput type="text" name="year" label="year" />
+          <FormSelect label="name" />
+          <Button htmlType="submit">Submit</Button>
+        </UniForm>
+      </Col>
+    </Flex>
   );
 };
 
