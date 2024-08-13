a340c95e25986b779b507639c969def61c5c3244
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-13 20:02:37 +0600

    added CreateAcademicSemester

---
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -0,0 +1,9 @@
+const CreateAcademicSemester = () => {
+  return (
+    <div>
+      <h4>CreateAcademicSemester</h4>
+    </div>
+  );
+};
+
+export default CreateAcademicSemester;
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -10,6 +10,7 @@ import {
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react";
 import { logout, setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -30,6 +31,10 @@ const baseQueryWithRefreshToken: BaseQueryFn<
   DefinitionType
 > = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    toast.error("User not found");
+  }
 
   if (result?.error?.status === 401) {
     //* Send Refresh
