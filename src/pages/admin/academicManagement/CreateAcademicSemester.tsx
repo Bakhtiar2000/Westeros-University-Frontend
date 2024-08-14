9a33d3ba0751ea706336062bb75a534f737d9e1f
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-15 00:45:31 +0600

    update

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -15,7 +15,6 @@ const CreateAcademicSemester = () => {
   }));
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
     const name = semesterOptions[Number(data?.name) - 1]?.label;
     const semesterData = {
       name,
