650662f0d58c7cfa3e06d728360388c13d80eb15
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-08-27 00:59:45 +0600

    started student course offering

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,4 +1,8 @@
+import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
+
 const OfferedCourse = () => {
+  const { data } = useGetAllOfferedCoursesQuery(undefined);
+  console.log(data);
   return (
     <div>
       <p>offered courses</p>
---
+++ src/redux/features/student/studentCourseManagement.api.ts
@@ -0,0 +1,44 @@
+import { TQueryParam, TResponseRedux } from "../../../types";
+import { TOfferedCourse } from "../../../types/studentCourse.type";
+import { baseApi } from "../../api/baseApi";
+
+const studentCourseManagement = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    //-----------------Get All Offered Courses-----------------
+    getAllOfferedCourses: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "offered-courses/me/my-offered-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TOfferedCourse[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+
+    //-----------------Add Students-----------------
+    addStudent: builder.mutation({
+      query: (data) => ({
+        url: "/users/create-student",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllOfferedCoursesQuery } = studentCourseManagement;
